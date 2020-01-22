9ad8a0bfb47a4d18f0eee1aeae29012bdce9b086
Jean-Pierre <jcornaglia@gmail.com>
2020-01-22 14:31:33 +0100

    merger error correction

--- tests/test_views.py
+++ tests/test_views.py
@@ -14,9 +14,16 @@ class TestViews(TestCase):
     def test_products_json(self):
         response = self.client.get("/api/v1/products")
         products = response.json
-        #print (len(products))
+        self.assertEqual(response.status_code, 200)
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_product_one_json(self):
+        response = self.client.get("/api/v1/products/2")
+        products = response.json
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(products,dict)
+        self.assertDictEqual(products, {'id': 2, 'name': 'Socialive.tv'})
 
     def test_product_delete_json_ok(self):
         response = self.client.delete("/api/v1/products/2")
@@ -29,3 +36,7 @@ class TestViews(TestCase):
         self.assertIsInstance(products, list)
         self.assertEqual(len(products), 2)
 
+    def test_product_delete_json_ko(self):
+        response = self.client.delete("/api/v1/products/20002")
+        products = response.json
+        self.assertEqual(response.status_code, 404)
