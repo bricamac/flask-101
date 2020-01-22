ed2796130a661124976dfe2c85df688fc796e465
Jean-Pierre <jcornaglia@gmail.com>
2020-01-22 16:12:35 +0100

    ajout de la fonction update

--- wsgi.py
+++ wsgi.py
@@ -37,7 +37,7 @@ def firstjson():
 
 
 @app.route('/api/v1/products/<int:product_id>', methods=['GET'])
-def readproduct(product_id):
+def read_product(product_id):
     for product in PRODUCTS:
         if product['id'] == product_id:
             # Product found...
@@ -46,7 +46,7 @@ def readproduct(product_id):
     return jsonify({'ERROR': 'product not found'}), 404
 
 @app.route('/api/v1/products/<int:product_id>' , methods=['DELETE'])
-def deleteproduct(product_id):
+def delete_product(product_id):
     for product in PRODUCTS:
         if product['id'] == product_id:
             # Product found...
@@ -56,16 +56,38 @@ def deleteproduct(product_id):
     return jsonify({'ERROR': 'product not found'}), 404
 
 @app.route('/api/v1/products' , methods=['POST'])
-def createproduct():
+def create_product():
     content = request.json
 
     #Format des datas...
     if "name" not in content:
         return jsonify({'ERROR': 'input data error'}), 422
+    if content["name"] == "" :
+        return jsonify({'ERROR': 'input data error'}), 422
+
     #prochain ID
     app_id.next()
     #ajout element
     PRODUCTS.append({'id':app_id.id,'name':content["name"]})
     return jsonify({'id':app_id.id }),201
+
+
+@app.route('/api/v1/products/<int:product_id>' , methods=['PATCH'])
+def update_product(product_id):
+    for product in PRODUCTS:
+        if product['id'] == product_id:
+            # Product found...
+            content = request.json
+            #Format des datas...
+            if "name" not in content:
+                return jsonify({'ERROR': 'input data error'}), 422
+            if content["name"] == "" :
+                return jsonify({'ERROR': 'input data error'}), 422
+
+            product["name"] = content["name"]
+            return "", 204
+
+    # Production not found
+    return jsonify({'ERROR': 'product not found'}), 404
 
 
