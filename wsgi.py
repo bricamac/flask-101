44b55dd7b5ecc1ff135138379d6dc341a52f29ae
Jean-Pierre <jcornaglia@gmail.com>
2020-01-22 10:55:33 +0100

    Add first tests

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+PRODUCTS = [
+    { 'id': 1, 'name': 'Skello' },
+    { 'id': 2, 'name': 'Socialive.tv' },
+    { 'id': 3, 'name': 'coca'}
+]
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def firstjson():
+    return jsonify(PRODUCTS)
