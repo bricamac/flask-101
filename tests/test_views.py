e9096825d9610e9ecd72d2404c28385569179746
Jean-Pierre <jcornaglia@gmail.com>
2020-01-22 12:30:39 +0100

    add delete function

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,31 @@
 # tests/test_views.py
 from flask_testing import TestCase
-from wsgi import app
+from wsgi import app, init_product
 
 class TestViews(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
+    def setUp(self):
+        init_product()
+
+
     def test_products_json(self):
         response = self.client.get("/api/v1/products")
         products = response.json
-        print (len(products))
+        #print (len(products))
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_product_delete_json_ok(self):
+        response = self.client.delete("/api/v1/products/2")
+        products = response.json
+        self.assertEqual(response.status_code, 204)
+
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        #print (len(products))
+        self.assertIsInstance(products, list)
+        self.assertEqual(len(products), 2)
+
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,20 @@
+#from copy import deepcopy
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-PRODUCTS = [
+INITIAL_PRODUCTS = [
     { 'id': 1, 'name': 'Skello' },
     { 'id': 2, 'name': 'Socialive.tv' },
     { 'id': 3, 'name': 'coca'}
 ]
+
+PRODUCTS = INITIAL_PRODUCTS.copy()
+
+#Pour les tests....
+def init_product():
+    PRODUCTS.clear()
+    PRODUCTS.extend(INITIAL_PRODUCTS)
+
 
 @app.route('/')
 def hello():
@@ -14,3 +23,14 @@ def hello():
 @app.route('/api/v1/products')
 def firstjson():
     return jsonify(PRODUCTS)
+
+@app.route('/api/v1/products/<int:product_id>' , methods=['DELETE'])
+def deleteproduct(product_id):
+
+    for product in PRODUCTS:
+        if product['id'] == product_id:
+            # Product found...
+            PRODUCTS.remove(product)
+            return "", 204
+    # Production not found
+    return jsonify({'ERROR': 'product not found'}), 404
