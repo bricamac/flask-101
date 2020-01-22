1783a965d364a4fcedebebadaefd57458a6e04b6
Jean-Pierre <jcornaglia@gmail.com>
2020-01-22 11:22:43 +0100

    add read function

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 PRODUCTS = [
-    { 'id': 1, 'name': 'Skello' },
-    { 'id': 2, 'name': 'Socialive.tv' },
-    { 'id': 3, 'name': 'coca'}
+    {'id': 1, 'name': 'Skello'},
+    {'id': 2, 'name': 'Socialive.tv'},
+    {'id': 3, 'name': 'coca'}
 ]
 
 @app.route('/')
@@ -14,3 +14,15 @@ def hello():
 @app.route('/api/v1/products')
 def firstjson():
     return jsonify(PRODUCTS)
+
+@app.route('/api/v1/products/<int:product_id>')
+def readproduct(product_id):
+
+    for product in PRODUCTS:
+        if product['id'] == product_id:
+            # Product found...
+            return jsonify(product)
+    # Production not found
+    return jsonify({'ERROR': 'product not found'}), 404
+
+
