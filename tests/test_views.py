58e6bbf792563f3259dd6e9830d75e4eb8c64b1f
Jean-Pierre <jcornaglia@gmail.com>
2020-01-22 15:32:59 +0100

    ajout de la fonction create

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,7 @@
 # tests/test_views.py
 from flask_testing import TestCase
 from wsgi import app, init_product
+import json
 
 class TestViews(TestCase):
     def create_app(self):
@@ -40,3 +41,19 @@ class TestViews(TestCase):
         response = self.client.delete("/api/v1/products/20002")
         products = response.json
         self.assertEqual(response.status_code, 404)
+
+    def test_product_create_ok(self):
+        response=self.client.post("/api/v1/products", data=json.dumps(dict(name="cafe")),content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        products = response.json
+        self.assertIsInstance(products,dict)
+
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        #print (len(products))
+        self.assertIsInstance(products, list)
+        self.assertEqual(len(products), 4)
+
+    def test_product_create_ko(self):
+        response=self.client.post("/api/v1/products", data=json.dumps(dict(truc="cafe")),content_type='application/json')
+        self.assertEqual(response.status_code, 422)
